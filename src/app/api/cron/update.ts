d9f1ae31479e117f3a7b4555db2fca141596ff4f
Slava Potravnyi <spotravnyy@paloaltonetworks.com>
2024-07-03 19:30:18 -0700

    feat: Add cron job to pull data every minute

---
+++ src/app/api/cron/update.ts
@@ -0,0 +1,16 @@
+import type { NextRequest } from 'next/server';
+import { libreRouter } from '~/server/api/routers/libre';
+import { createCallerFactory, createTRPCContext } from '~/server/api/trpc';
+ 
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+  const createCaller = createCallerFactory(libreRouter);
+  const TRPCContext = await createTRPCContext({headers: request.headers})
+  const caller = createCaller(TRPCContext)
+  return Response.json(await caller.update());
+}
