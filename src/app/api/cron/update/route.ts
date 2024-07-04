eaa87843098c60743af911824beca7bb78e07254
Slava Potravnyi <spotravnyy@paloaltonetworks.com>
2024-07-03 19:49:04 -0700

    chore: Remove debug logs

--- src/app/api/cron/update/route.ts
+++ src/app/api/cron/update/route.ts
@@ -3,7 +3,6 @@ import { libreRouter } from "~/server/api/routers/libre";
 import { createCallerFactory, createTRPCContext } from "~/server/api/trpc";
 
 export async function GET(request: NextRequest) {
-	console.log("Cron job: start");
 	const authHeader = request.headers.get("authorization");
 	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
 		console.log("Cron job: 401");
@@ -14,8 +13,5 @@ export async function GET(request: NextRequest) {
 	const createCaller = createCallerFactory(libreRouter);
 	const TRPCContext = await createTRPCContext({ headers: request.headers });
 	const caller = createCaller(TRPCContext);
-	console.log("Cron job: caller created");
-	const res = await caller.update();
-	console.log("Cron job: done");
-	return Response.json(res);
+	return Response.json(await caller.update());
 }
