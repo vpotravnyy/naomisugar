e9e945171cbc4b91279b2c19aa8d95af84558291
Slava Potravnyi <spotravnyy@paloaltonetworks.com>
2024-07-03 19:39:26 -0700

    fix: Debug cron job

--- src/app/api/cron/update.ts
+++ src/app/api/cron/update.ts
@@ -1,16 +1,21 @@
-import type { NextRequest } from 'next/server';
-import { libreRouter } from '~/server/api/routers/libre';
-import { createCallerFactory, createTRPCContext } from '~/server/api/trpc';
- 
+import type { NextRequest } from "next/server";
+import { libreRouter } from "~/server/api/routers/libre";
+import { createCallerFactory, createTRPCContext } from "~/server/api/trpc";
+
 export async function GET(request: NextRequest) {
-  const authHeader = request.headers.get('authorization');
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response('Unauthorized', {
-      status: 401,
-    });
-  }
-  const createCaller = createCallerFactory(libreRouter);
-  const TRPCContext = await createTRPCContext({headers: request.headers})
-  const caller = createCaller(TRPCContext)
-  return Response.json(await caller.update());
+	console.log("Cron job: start");
+	const authHeader = request.headers.get("authorization");
+	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+		console.log("Cron job: 401");
+		return new Response("Unauthorized", {
+			status: 401,
+		});
+	}
+	const createCaller = createCallerFactory(libreRouter);
+	const TRPCContext = await createTRPCContext({ headers: request.headers });
+	const caller = createCaller(TRPCContext);
+	console.log("Cron job: caller created");
+	const res = await caller.update();
+	console.log("Cron job: done");
+	return Response.json(res);
 }
