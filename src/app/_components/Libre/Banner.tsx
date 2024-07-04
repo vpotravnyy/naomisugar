2e5a0861f932ce334f151e55e8c2dd45741c1f2f
Slava Potravnyi <spotravnyy@paloaltonetworks.com>
2024-07-04 16:38:02 -0700

    fix: Added time

--- src/app/_components/Libre/Banner.tsx
+++ src/app/_components/Libre/Banner.tsx
@@ -22,11 +22,57 @@ const arrowStyle = {
 	SingleUp: { transform: "rotate(0deg)" },
 	NotComputable: { display: "none" },
 };
+const millisecondsPerSecond = 1000;
+const secondsPerMinute = 60;
+const minutesPerHour = 60;
+const hoursPerDay = 24;
+const daysPerWeek = 7;
+const relativeDateFormat = new Intl.RelativeTimeFormat("en", { style: "long" });
 
+function formatRelativeTime(createTime: Date) {
+	const diff = +createTime - +new Date();
+	const intervals = {
+		week:
+			millisecondsPerSecond *
+			secondsPerMinute *
+			minutesPerHour *
+			hoursPerDay *
+			daysPerWeek,
+		day:
+			millisecondsPerSecond * secondsPerMinute * minutesPerHour * hoursPerDay,
+		hour: millisecondsPerSecond * secondsPerMinute * minutesPerHour,
+		minute: millisecondsPerSecond * secondsPerMinute,
+		second: millisecondsPerSecond,
+	};
+	for (const interval in intervals) {
+		const i =
+			intervals[interval as "week" | "day" | "hour" | "minute" | "second"];
+		if (i <= Math.abs(diff)) {
+			return relativeDateFormat.format(
+				Math.trunc(diff / i),
+				interval as "week" | "day" | "hour" | "minute" | "second",
+			);
+		}
+	}
+	return relativeDateFormat.format(diff / 1000, "second");
+}
 export default function Banner({ current }: { current: TLibreDataPoint }) {
 	const tooltipData = useTooltipData();
 	const value = tooltipData ? Number(tooltipData?.y as number) : current?.value;
 	const trend = tooltipData ? tooltipData.trend : current?.trend;
+	const date = tooltipData
+		? new Date(tooltipData.x).toLocaleString("en", {
+				hour: "numeric",
+				minute: "2-digit",
+			})
+		: current.date.toLocaleString("en", {
+				hour: "numeric",
+				minute: "2-digit",
+			});
+	const since = formatRelativeTime(
+		tooltipData ? new Date(tooltipData.x) : current.date,
+	);
+	new Intl.RelativeTimeFormat("en", { style: "short" });
 
 	let color = "";
 	if (Number(value) < LOW.MAX) {
@@ -49,7 +95,10 @@ export default function Banner({ current }: { current: TLibreDataPoint }) {
 					{value?.toFixed(1) ?? "---"}
 					<ArrowUpIcon className="w-12 h-12 ml-2" style={arrowStyle[trend]} />
 				</div>
-				<span className="text-lg">mmol/L</span>
+				<div className="flex flex-row items-center justify-center gap-2">
+					<span className="text-lg">{date}</span>
+					<span className="text-sm">({since})</span>
+				</div>
 			</div>
 		</section>
 	);
--- src/server/api/routers/libre.ts
+++ src/server/api/routers/libre.ts
@@ -1,66 +1,84 @@
 import { type SQL, getTableColumns, sql } from "drizzle-orm";
-import type { InferSelectModel } from 'drizzle-orm'
+import type { InferSelectModel } from "drizzle-orm";
 import type { PgTable } from "drizzle-orm/pg-core";
-import { omit } from "lodash"
+import { omit } from "lodash";
 import { env } from "~/env";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { libre, libreCurrent } from "~/server/db/schema";
 import { LibreLinkUpClient } from "~/server/lib/libre";
 
 const buildConflictUpdateColumns = <
-  T extends PgTable,
-  Q extends keyof T['_']['columns']
+	T extends PgTable,
+	Q extends keyof T["_"]["columns"],
 >(
-  table: T,
-  columns: Q[],
+	table: T,
+	columns: Q[],
 ) => {
-  const cls = getTableColumns(table);
-  return columns.reduce((acc, column) => {
-    const colName = cls[column]?.name;
-    acc[column] = sql.raw(`excluded.${colName}`);
-    return acc;
-  }, {} as Record<Q, SQL>);
+	const cls = getTableColumns(table);
+	return columns.reduce(
+		(acc, column) => {
+			const colName = cls[column]?.name;
+			acc[column] = sql.raw(`excluded.${colName}`);
+			return acc;
+		},
+		{} as Record<Q, SQL>,
+	);
 };
 
-export type TLibreDataPoint = InferSelectModel<typeof libre>
-export type TLibreResponse = { current: TLibreDataPoint, history: TLibreDataPoint[]}
+export type TLibreDataPoint = InferSelectModel<typeof libre>;
+export type TLibreResponse = {
+	current: TLibreDataPoint;
+	history: TLibreDataPoint[];
+};
 
 export const libreRouter = createTRPCRouter({
-  read: publicProcedure.query(async ({ ctx }): Promise<TLibreResponse> => {
-    const current = await ctx.db.query.libreCurrent.findFirst();
-    const history = await ctx.db.query.libre.findMany({
-      orderBy: (libre, { desc }) => [desc(libre.date)],
-      limit: 1000
-    });
+	read: publicProcedure.query(async ({ ctx }): Promise<TLibreResponse> => {
+		const current = await ctx.db.query.libreCurrent.findFirst();
+		const history = await ctx.db.query.libre.findMany({
+			orderBy: (libre, { desc }) => [desc(libre.date)],
+			limit: 1000,
+		});
 
-    return {
-      current: omit(current, ['id']),
-      history
-    }
-  }),
+		return {
+			current: omit(current, ["id"]),
+			history,
+		};
+	}),
 
-  update: publicProcedure.mutation(async ({ ctx }) => {
-    const oldCurrent = await ctx.db.query.libreCurrent.findFirst()
-    if (!oldCurrent) return { success: false, error: 'No current data found' };
+	update: publicProcedure.mutation(async ({ ctx }) => {
+		const oldCurrent = await ctx.db.query.libreCurrent.findFirst();
+		if (!oldCurrent) return { success: false, error: "No current data found" };
 
-    if (+new Date() - +new Date(oldCurrent.date) < 60 * 1000) {
-      return { success: false, error: 'Cannot update more than once per minute' };
-    }
+		if (+new Date() - +new Date(oldCurrent.date) < 60 * 1000) {
+			return {
+				success: false,
+				error: "Cannot update more than once per minute",
+			};
+		}
 
-    const libreClient = LibreLinkUpClient({ username: env.LIBRE_USERNAME, password: env.LIBRE_PASSWORD });
-    const { current, history } = await libreClient.read();
+		const libreClient = LibreLinkUpClient({
+			username: env.LIBRE_USERNAME,
+			password: env.LIBRE_PASSWORD,
+		});
+		const { current, history } = await libreClient.read();
+		console.log("history", history);
 
-    // eslint-disable-next-line drizzle/enforce-update-with-where
-    await ctx.db.update(libreCurrent).set({ ...current })
-      
-    await ctx.db
-      .insert(libre)
-      .values(history)
-      .onConflictDoUpdate({
-        target: libre.date,
-        set: buildConflictUpdateColumns(libre, ['value', 'is_low', 'is_high', 'trend'])
-      });
+		// eslint-disable-next-line drizzle/enforce-update-with-where
+		await ctx.db.update(libreCurrent).set({ ...current });
 
-    return { success: true };
-  })
+		await ctx.db
+			.insert(libre)
+			.values(history)
+			.onConflictDoUpdate({
+				target: libre.date,
+				set: buildConflictUpdateColumns(libre, [
+					"value",
+					"is_low",
+					"is_high",
+					"trend",
+				]),
+			});
+
+		return { success: true };
+	}),
 });
